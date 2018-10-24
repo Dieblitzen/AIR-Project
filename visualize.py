b7b18dd95b98b708e06c6d80b268528ebfc165b7
Samar Khanna <sak296@cornell.edu>
2018-10-24 17:36:48 -0400

    Visualize working kind of

--- visualize.py
+++ visualize.py
@@ -5,6 +5,7 @@ import os
 import matplotlib.patches as patches
 import get_bounding_boxes
 import merge_data
+from shapely.geometry.polygon import Polygon
 
 ## Takes file name and converts to np array
 def tiff2array(filename):
@@ -21,7 +22,7 @@ def image_to_np_array(image_folder): # Fetches images from download folder
     ind = 0
     for filename in os.listdir(image_folder):
         if filename.endswith(".tiff"): 
-            path_to_file = path + '/' + filename
+            path_to_file = image_folder + '/' + filename
             images_dict[ind] = tiff2array(path_to_file)
             ind += 1
     ## Return rgb image in np array format
@@ -45,6 +46,5 @@ lon_max = -73.7582464736
 bboxes = get_bounding_boxes.get_bounding_boxes(lat_min,lon_min,lat_max,lon_max)
 im_array = image_to_np_array("./downloads")
 
-
 visualize_bounding_boxes(im_array, 
-                merge_data.OSM_to_pixels([lat_min,lon_min,lat_max,lon_max],im_array.size[:2], bboxes))
+                merge_data.OSM_to_pixels([lat_min,lon_min,lat_max,lon_max],im_array.shape[:2], bboxes))
