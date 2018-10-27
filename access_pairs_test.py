dbfe318eec902d55ec975b76d6ba4b8a8eb03136
Adam Skrocki <adamskrocki@dhcp-rhodes-418.eduroam.cornell.edu>
2018-10-27 13:40:33 -0400

    Cleaned up access_pairs_test.py

--- access_pairs_test.py
+++ access_pairs_test.py
@@ -93,29 +93,3 @@ z.extractall(path)
 def get_datalayers():
     res = requests.get(f'{server}/v2/datalayers/',auth=auth)
     print(res.json)
-
-
-## Takes file name and converts to np array
-def tiff2array(fn):
-    ## Special gdal dataset
-    dataset = gdal.Open(fn)
-    array = np.array(dataset.GetRasterBand(1).ReadAsArray(), np.uint8)
-    return array
-
-### TODO Make sure rgb layers are in the right order in returned value. 
-def image_to_np_array(image_folder): # Fetches images from download folder 
-    ## Will go through filenames and put the image arrays in images_dict
-    images_dict = {}
-    ## Loop through files in downloads directory (if multiple)
-    ind = 0
-    for filename in os.listdir(image_folder):
-        if filename.endswith(".tiff"): 
-            path_to_file = path + '/' + filename
-            images_dict[ind] = tiff2array(path_to_file)
-            ind += 1
-    ## Return rgb image in np array format
-    return np.transpose(np.array(list(images_dict.values())))
-
-print("numpy array result: ")
-print(image_to_np_array(path))
-
