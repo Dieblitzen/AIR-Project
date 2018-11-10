900fadc5f72c9b59d2812968ede5707f555f9933
Nolan Gray <nsg58@cornell.edu>
2018-11-10 00:22:53 -0500

    Added function that tiles our images

--- tile.py
+++ tile.py
@@ -1,9 +1,12 @@
-## Data processising 
+## Data processising
 
-import numpy as np 
+import numpy as np
 import get_bounding_boxes
+import visualize
+from sklearn.feature_extraction import image
 
 
 def tile_image(entire_image, tile_size):
-    tiled_images = []
-    
+
+    tiled_images = image.extract_patches_2d(entire_image, (tile_size, tile_size))
+    return tiled_images
