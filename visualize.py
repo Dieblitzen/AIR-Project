f6f4f69bcf87a78587eb9fc1fdb476b195af9419
Adam Skrocki <ams792@cornell.edu>
2018-11-14 18:31:27 -0500

    Updated so no need to rotate anymore

--- visualize.py
+++ visualize.py
@@ -9,60 +9,65 @@ import scipy.misc
 import tile
 
 
-## Takes file name and converts to np array
+# Takes file name and converts to np array
 def tiff2array(filename):
-    ## Special gdal dataset
+    # Special gdal dataset
     dataset = gdal.Open(filename)
     array = np.array(dataset.GetRasterBand(1).ReadAsArray(), np.uint8)
-    print("array shape: ")
-    print(array.shape)
+
     return array
 
-### TODO Make sure rgb layers are in the right order in returned value. 
-def image_to_np_array(image_folder): # Fetches images from download folder 
-    ## Will go through filenames and put the image arrays in images_dict
-    images_dict = {}
-    ## Loop through files in downloads directory (if multiple)
-    ind = 0
+# TODO Make sure rgb layers are in the right order in returned value.
+
+
+def image_to_np_array(image_folder):  # Fetches images from download folder
+    # Will go through filenames and put the image arrays in images_dict
+    images_arr = []
+    # Loop through files in downloads directory (if multiple)
     for filename in os.listdir(image_folder):
-        if filename.endswith(".tiff"): 
+        if filename.endswith(".tiff"):
             path_to_file = image_folder + '/' + filename
-            images_dict[ind] = tiff2array(path_to_file)
-            ind += 1
-    ## Return rgb image in np array format
-    im_arr = np.transpose(np.array(list(images_dict.values())))
-    print("im array shape")
-    print(im_arr.shape)
+            images_arr.append(tiff2array(path_to_file))
+    # Return rgb image in np array format
+    im_arr = np.dstack(images_arr)
     arr2jpg(im_arr, 'images')
     return im_arr
 
+
 def arr2jpg(arr, folder):
-    ## Turns np array into jpg and saves into folder specified by folder
+    # Turns np array into jpg and saves into folder specified by folder
     scipy.misc.imsave(f'./{folder}/PAIRS_Area.jpg', arr)
 
-## TODO: add option to graph on image at certain path (or maybe just give np array?)
-def visualize_bounding_boxes(image_array, bb_pixels, YOLO=True): 
-    ## Visualize bounding boxes on an image with bb_pixels either as horizontal boxes 
+# TODO: add option to graph on image at certain path (or maybe just give np array?)
+
+
+def visualize_bounding_boxes(image_array, bb_pixels, YOLO=True):
+    # Visualize bounding boxes on an image with bb_pixels either as horizontal boxes
     # print(image_array[50])
     plt.imshow(image_array)
 
-    if (not YOLO) :
-        for bbox_coords in bb_pixels: 
+    if (not YOLO):
+        for bbox_coords in bb_pixels:
             poly = Polygon(bbox_coords)
-            x,y = poly.exterior.xy
-            plt.plot(x,y)
-    else: 
-        for bbox_coords in bb_pixels: 
-            bottom_left = (bbox_coords[0]-(bbox_coords[2]/2), bbox_coords[1]- (bbox_coords[3]/2))
-            bottom_right = (bbox_coords[0]+(bbox_coords[2]/2), bbox_coords[1]- (bbox_coords[3]/2))
-            top_left = (bbox_coords[0]-(bbox_coords[2]/2), bbox_coords[1]+(bbox_coords[3]/2))
-            top_right = (bbox_coords[0]+(bbox_coords[2]/2), bbox_coords[1]+(bbox_coords[3]/2))
+            x, y = poly.exterior.xy
+            plt.plot(x, y)
+    else:
+        for bbox_coords in bb_pixels:
+            bottom_left = (bbox_coords[0]-(bbox_coords[2]/2),
+                           bbox_coords[1] - (bbox_coords[3]/2))
+            bottom_right = (
+                bbox_coords[0]+(bbox_coords[2]/2), bbox_coords[1] - (bbox_coords[3]/2))
+            top_left = (bbox_coords[0]-(bbox_coords[2]/2),
+                        bbox_coords[1]+(bbox_coords[3]/2))
+            top_right = (bbox_coords[0]+(bbox_coords[2]/2),
+                         bbox_coords[1]+(bbox_coords[3]/2))
 
-            coords = [bottom_left,top_left,top_right,bottom_right]
+            coords = [bottom_left, top_left, top_right, bottom_right]
             poly = Polygon(coords)
-            x,y = poly.exterior.xy
-            plt.plot(x,y)
+            x, y = poly.exterior.xy
+            plt.plot(x, y)
     plt.show()
+
 
 # get the image as an array
 im_array = image_to_np_array("./downloads")
@@ -70,34 +75,24 @@ im_array = image_to_np_array("./downloads")
 # get bounding boxes from OSM
 bboxes = get_bounding_boxes.get_bounding_boxes(YOLO=True)
 # print(len(bboxes)) # We have 2990 buildings. Seems like more than enough
-pixels = get_bounding_boxes.OSM_to_pixels(im_array.shape[:2], bboxes, YOLO=True)
+pixels = get_bounding_boxes.OSM_to_pixels(
+    im_array.shape[:2], bboxes, YOLO=True)
+
+visualize_bounding_boxes(im_array, pixels, YOLO=True)
 
 
-# tile the image, which returns a list
-tile_list = tile.tile_image(np.rot90(im_array, 1), pixels, 228)
+# # tile the image, which returns a list
+# tile_list = tile.tile_image(np.rot90(im_array, 1), pixels, 228)
 
-# for each tile, visualize boxes on it
-for i in range(len(tile_list)):
-    elts = tile_list[i]
-    tile_image = elts[0]
-    bboxes_list = elts[1]
-    # print("bounding boxes list for image: " + str(i))
-    # print(bboxes_list)
-    visualize_bounding_boxes(tile_image, bboxes_list)
-    
-
-
+# # for each tile, visualize boxes on it
+# for i in range(len(tile_list)):
+#     elts = tile_list[i]
+#     tile_image = elts[0]
+#     bboxes_list = elts[1]
+#     # print("bounding boxes list for image: " + str(i))
+#     # print(bboxes_list)
+#     visualize_bounding_boxes(tile_image, bboxes_list)
 
 
 # # Size of image (3648, 5280, 3)
 # # We are aiming for (3648, 5244, 3), as this will divide by 228 (our target image size)
-
-# im_array = image_to_np_array("./downloads")
-# print()
-
-# pixels = get_bounding_boxes.OSM_to_pixels(im_array.shape[:2], bboxes, YOLO=True)
-
-# visualize_bounding_boxes(np.rot90(im_array,1), 
-#                pixels,
-#                YOLO=True)
-
