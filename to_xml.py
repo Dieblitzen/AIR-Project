fc85fd71c746e4aa54f75bbd44b3542efa6e74de
Samar Khanna <sak296@cornell.edu>
2018-11-28 18:06:51 -0500

    Wrote code to convert bbox for one image to xml

--- to_xml.py
+++ to_xml.py
@@ -1,67 +1,64 @@
 from lxml import etree
 
 
+def bbox_for_image(file_name, im_size, bboxes):
+		# Header.
+		annotation = etree.Element('annotation')
 
-## Create XML
-annotation = etree.Element('annotation')
-# Filename 
-filename = etree.Element('filename')
-filename.text = file ## VAR 
+		# Filename 
+		filename = etree.Element('filename')
+		filename.text = file_name
 
-# Size
-size = etree.Element('size')
-# nested elements in size
-width = etree.Element('width')
-height = etree.Element('height')
-depth = etree.Element('depth')
-width.text = ""
-height.text = ""
-depth.text = ""
-# append nested elements in size 
-size.append(width)
-size.append(height)
-size.append(depth)
+		# Image size
+		size = etree.Element('size')
+		# nested elements in size
+		width = etree.Element('width')
+		height = etree.Element('height')
+		depth = etree.Element('depth')
+		width.text = str (im_size[1])
+		height.text = str (im_size[0])
+		depth.text = str (im_size[2])
 
-bndbox = 
+		size.append(width)
+		size.append(height)
+		size.append(depth)
 
-# append filename
-annotation.append(filename)
-# append size
-annotation.append(size)
+		annotation.append(filename)
+		annotation.append(size)
 
+		for bbox in bboxes: 
+				# object for each bounding box
+				obj = etree.Element('object')
 
+				# Always buildings we're detecting (name is label)
+				name = etree.Element('name')
+				name.text = "building"
 
+				# Bounding box preocessing. We assume that bboxes are in
+				# [centreX, centreY, width, height] format, and convert it to
+				# x_min, x_max, y_min, y_max
+				bndbox = etree.Element('bndbox')
+				xmin = etree.Element('xmin')
+				xmin.text = str (bbox[0] - bbox[2]/2)
 
+				ymin = etree.Element('ymin')
+				ymin.text = str (bbox[1] - bbox[3]/2)
 
-<annotation>
-	<filename>000001.jpg</filename>
-	<size>
-		<width>353</width>
-		<height>500</height>
-		<depth>3</depth>
-	</size>
-	<object>
-		<name>dog</name>
-		<pose>Left</pose>
-		<truncated>1</truncated>
-		<difficult>0</difficult>
-		<bndbox>
-			<xmin>48</xmin>
-			<ymin>240</ymin>
-			<xmax>195</xmax>
-			<ymax>371</ymax>
-		</bndbox>
-	</object>
-	<object>
-		<name>person</name>
-		<pose>Left</pose>
-		<truncated>1</truncated>
-		<difficult>0</difficult>
-		<bndbox>
-			<xmin>8</xmin>
-			<ymin>12</ymin>
-			<xmax>352</xmax>
-			<ymax>498</ymax>
-		</bndbox>
-	</object>
-</annotation>
+				xmax = etree.Element('xmax')
+				xmax.text = str (bbox[0] + bbox[2]/2)
+
+				ymax = etree.Element('ymax')
+				ymax.text = str (bbox[1] + bbox[3]/2)
+
+				bndbox.append(xmin)
+				bndbox.append(ymin)
+				bndbox.append(xmax)
+				bndbox.append(ymax)
+
+				# append nested elements in obj
+				obj.append(name)
+				obj.append(bndbox)
+
+				annotation.append(obj)
+
+		return annotation
