Added angle calculation and fixed decimals

get_bounding_boxes.py
@@ -88,14 +88,14 @@ def get_pixor_box_dimensions(bbox):
     corner, closest, second_closest, sorted_distances = get_two_closest_points(bbox)
     vector = np.array(np.subtract(second_closest,corner)) if corner[0] > second_closest[0] else np.array(np.subtract(corner,second_closest))
     unit_vector = vector / np.linalg.norm(vector)
-    width, length = sorted_distances[1][1], sorted_distances[2][1]
+    width, length = math.floor(sorted_distances[1][1]), math.floor(sorted_distances[2][1])
     return np.arccos(np.clip(np.dot(unit_vector, (1,0)), -1.0, 1.0)), width, length
 
 def get_pixor_center(bbox):
     sorted_by_lat = sorted(bbox, key= lambda pair: pair[0])
     sorted_by_lon = sorted(bbox, key= lambda pair: pair[1])
-    center_x = (sorted_by_lon[0][1] + sorted_by_lon[3][1])/2
-    center_y = (sorted_by_lat[0][0] + sorted_by_lat[3][0])/2
+    center_x = math.floor((sorted_by_lon[0][1] + sorted_by_lon[3][1])/2)
+    center_y = math.floor((sorted_by_lat[0][0] + sorted_by_lat[3][0])/2)
     return center_x, center_y
 
 def corner_boxes_in_pixels(image_size, buildings):

pixor/network.py
@@ -122,15 +122,44 @@ output_box = tf.nn.relu(conv2d(input=header4, filter_size=3, in_channels=96, out
 output_class = tf.nn.relu(conv2d(input=header4, filter_size=3, in_channels=96, out_channels=6, stride=1) + b_conv1)
 
 
-#Cost function
-cross_entropy_box = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_box, logits=output_box))
-cross_entropy_class = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_class, logits=output_class))
-cross_entropy = cross_entropy_box + cross_entropy_class
+# DEFINING LOSS
+
+def smooth_L1(box_labels, box_preds):
+  # calculate difference between box_labels and box_preds
+  difference = tf.math.subtract(box_preds, box_labels)
+
+  # if absolute value of difference < 1
+    # 0.5 * (abs(difference))^2
+  
+  # otherwise
+    # abs(difference) - 0.5
+
+    
+
+class_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_class, logits=output_class))
+box_loss = smooth_L1(box_label=y_box, box_pred=output_box)
+pixor_loss = class_loss + box_loss
+
 #A step to minimize our cost function
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(pixor_loss)
 #List of booleans, indicating whether or not we guessed the correct digit
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 #Calculates overall occuracy on test set
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+
 with tf.Session() as sess:
+  #initialize everything
+  sess.run(tf.global_variables_initializer())
+  num_epochs = 10
+  
+  for epoch in range(num_epochs):
+
+    num_batches = None
+    for batch_number in num_batches:
+      # obtain batch
+      batch = None
+
+      # update
+      train_step.run(feed_dict = {x: batch[0], y_box: batch[1], 
+                                  y_class: batch[2], keep_prob: 0.5})

tile.py
@@ -33,7 +33,7 @@ def boxes_in_tile_pixor(bboxes, corner_boxes, row_start, row_end, col_start, col
     # gets the boxes within this tile, with coordinates relative to tile
     boxes_within_tile, selected_indices = boxes_in_tile(bboxes, row_start, row_end, col_start, col_end)
 
-    pixel_labels = np.zeros((228, 228, 6))
+    pixel_labels = np.zeros((228, 228, 7))
 
     print("looking at new tile")
     for r in range(row_start, row_end):
@@ -49,19 +49,19 @@ def boxes_in_tile_pixor(bboxes, corner_boxes, row_start, row_end, col_start, col
                 pixel = (r, c)
 
                 if inside_box(pixel, corner_boxes[selected_indices[bbox_index]], entire_img_shape):
-                    new_dx = abs(pixel[0] - bboxes[bbox_index][0])
-                    new_dy = abs(pixel[1] - bboxes[bbox_index][1])
+                    new_dx = abs(pixel[0] - boxes_within_tile[bbox_index][0])
+                    new_dy = abs(pixel[1] - boxes_within_tile[bbox_index][1])
+                    
                     if(np.sqrt(new_dx**2 + new_dy**2) <= np.sqrt(dx**2 + dy**2)):
                         dx = new_dx
                         dy = new_dy
-                        heading, width, length = bboxes[bbox_index][2:]
+                        heading, width, length = boxes_within_tile[bbox_index][2:]
                         in_a_box = 1
 
             new_r = r - row_start
             new_c = c - col_start
-            pixel_labels[new_r, new_c,:] = [dx, dy, heading, width, length, in_a_box]
-    return pixel_labels
-
+            pixel_labels[new_r, new_c,:] = [int(dx), int(dy), np.cos(heading), np.sin(heading), int(width), int(length), in_a_box]
+            
 
 # Takes array representing entire queried image and bounding boxes (with pixel coordinates) relative to
 # entire image, and outputs a list of tuples where the first element is the tiled image and the second