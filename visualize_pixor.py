6ce9f63f175ec8dce7e3600724666efb7ab31a8a
Nolan Gray <nsg58@cornell.edu>
2018-12-03 02:25:25 -0500

    added more to visualize, almost finished

--- visualize_pixor.py
+++ visualize_pixor.py
@@ -13,8 +13,35 @@ from data_extract import extract_data
 
 # TODO: add option to graph on image at certain path (or maybe just give np array?)
 
+def extract_positive_labels(bboxes):
+    return [box for box in bboxes if box[-1] != 0. ]
+#applies translation, rotation, then un-translation of four points
+def rotate_points(point, center_x, center_y, cos_angle, sin_angle):
+    temp_x = point[0] - center_x
+    temp_y = point[1] - center_y
+    rotated_x = temp_x*cos_angle - temp_y*sin_angle
+    rotatedY = tempX*sin(theta) + tempY*cos(theta)
+#Converts pixor description of a box into four coordinates.
+def pixor_to_corners(box):
+    # float tempX = x - cx;
+    # float tempY = y - cy;
+    #
+    # // now apply rotation
+    # float rotatedX = tempX*cos(theta) - tempY*sin(theta);
+    # float rotatedY = tempX*sin(theta) + tempY*cos(theta);
+    #
+    # // translate back
+    # x = rotatedX + cx;
+    # y = rotatedY + cy;
+    center_x, center_y, cos_angle, sin_angle, width, length = box
+    four_corners = [(center_x+width, center_y+height),
+        (center_x+width, center_y-height),
+        (center_x-width, center_y-height),
+        (center_x-width, center_y+height)]
+    rotated_corners = [rotate_point(corner, center_x, center_y, cos_angle, sin_angle) for corner in four_corners]
 
-def visualize_bounding_boxes(image_array, bb_pixels):
+
+def visualize_bounding_boxes(image_array, bboxes):
     # Visualize bounding boxes on an image with bb_pixels either as horizontal boxes
     # print(image_array[50])
     plt.imshow(image_array)
@@ -28,6 +55,11 @@ def visualize_bounding_boxes(image_array, bb_pixels):
     # print("number of pixor boxes: " + str(non_zero))
 
 
+    for box in bboxes:
+        coordinates = pixor_to_corners(box)
+        poly = Polygon(coordinates)
+        x, y = poly.exterior.xy
+        plt.plot(x, y)
     for r in range(0, bb_pixels.shape[0]):
       for c in range(0, bb_pixels.shape[1]):
         pot_box = bb_pixels[r, c]
@@ -36,13 +68,6 @@ def visualize_bounding_boxes(image_array, bb_pixels):
           poly = Polygon([pot_box[0], pot_box[1]])
           x, y = poly.exterior.xy
           plt.plot(x, y)
-
-        # for bbox_coords in bb_pixels:
-        #     poly = Polygon(bbox_coords)
-        #     x, y = poly.exterior.xy
-        #     plt.plot(x, y)
-
-
 
     # fig, ax = plt.subplots()
     # myInterval=228
@@ -77,12 +102,9 @@ for i in range(0, classlabels.shape[0]):
 # for each tile, visualize boxes on it
 for i in range(len(images)):
     image = images[i]
+    boxes_in_image = extract_positive_labels(boxlabels[i])
 
-    elts = tile_list[i]
-    tile_image = elts[0]
-    pixel_to_box_matrix = elts[1]
-
-    visualize_bounding_boxes(tile_image, pixel_to_box_matrix)
+    visualize_bounding_boxes(tile_image, boxes_in_image)
 
 
 # # Size of image (3648, 5280, 3)
