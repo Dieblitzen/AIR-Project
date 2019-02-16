1695313d0e946cfeb1b318129d2abd25b585355e
Samar Khanna <sak296@cornell.edu>
2019-02-16 15:32:18 -0500

    changed indentation

--- Dataset.py
+++ Dataset.py
@@ -20,51 +20,51 @@ from shapely.geometry.polygon import Polygon
 
 
 class Dataset:
+  """
+  The Dataset class provides an interface for working with a dataset consisting 
+  of images and annotations. 
+  """
+
+  def __init__(self, data_path):
     """
-    The Dataset class provides an interface for working with a dataset consisting 
-    of images and annotations. 
+    Initializes a dataset object using the data in path_to_data. 
     """
+    self.data_path = data_path
 
-    def __init__(self, data_path):
-        """
-        Initializes a dataset object using the data in path_to_data. 
-        """
-        self.data_path = data_path
+  def get_length(self):
+    """
+    Gets the length of the dataset (number of annotated images)
+    """
+    # All images in dataset
+    im_list = [name for name in os.listdir(
+        f"./{self.data_path}/img") if os.path.isfile(name)]
 
-    def get_length(self):
-        """
-        Gets the length of the dataset (number of annotated images)
-        """
-        # All images in dataset
-        im_list = [name for name in os.listdir(
-            f"./{self.data_path}/img") if os.path.isfile(name)]
+    return len(im_list)
 
-        return len(im_list)
+  def get_img_size(self):
+    """
+    Gets the size of the images in the dataset
+    """
+    # Gets first image in dataset
+    im = Image.open(os.listdir(f"./{self.data_path}/img")[0])
+    # Returns the shape of the image
+    return np.array(im).shape
 
-    def get_img_size(self):
-        """
-        Gets the size of the images in the dataset
-        """
-        # Gets first image in dataset
-        im = Image.open(os.listdir(f"./{self.data_path}/img")[0])
-        # Returns the shape of the image
-        return np.array(im).shape
+  def visualize_tile_with_annotation(self, tile):
+    """
+    Provides a visualization of the tile with filename tile and its 
+    corresponding annotation. 
+    """
+    im = Image.open(os.listdir(f"./{self.data_path}/{tile}"))
+    plt.imshow(im)
 
-    def visualize_tile_with_annotation(self, tile):
-        """
-        Provides a visualization of the tile with filename tile and its 
-        corresponding annotation. 
-        """
-        im = Image.open(os.listdir(f"./{self.data_path}/{tile}"))
-        plt.imshow(im)
+    # TODO: Visualize bounding boxes from json format.
 
-        # TODO: Visualize bounding boxes from json format.
+    plt.show()
 
-        plt.show()
-
-    def visualize_dataset(self):
-        """
-        Provides visualization of entire dataset image area, 
-        including annotations
-        """
-        pass
+  def visualize_dataset(self):
+    """
+    Provides visualization of entire dataset image area, 
+    including annotations
+    """
+    pass
