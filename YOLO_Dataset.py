f39fb4230dcc10115cdd1d18bc082787c63a582d
Samar Khanna <sak296@cornell.edu>
2019-02-20 16:10:43 -0500

    Created new file to make the dataset pipeline for YOLO

---
+++ YOLO_Dataset.py
@@ -0,0 +1,26 @@
+import Dataset
+import os
+import numpy as np
+import scipy.misc
+import math
+from PIL import Image
+import json
+import pickle
+
+# Visualising
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+from shapely.geometry.polygon import Polygon
+
+class YOLO_Dataset(Dataset):
+  """
+  The 'YOLO_Dataset' class inherits from the parent 'Dataset' class and provides
+  functionality to convert the images and annotations into the format required 
+  by the YOLO architecture (implemented as in Darkflow link: https://github.com/thtrieu/darkflow)
+
+  An object of 'YOLO_Dataset' wil provide the following functionality:
+
+  
+
+  """
+  pass
