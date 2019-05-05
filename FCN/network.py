8d6e1b9efe6aa1f14ffc2ccbc635913da89beb9b
Samar Khanna <sak296@cornell.edu>
2019-05-04 22:51:11 -0400

    Added logging and validation loss capabilities

--- FCN/network.py
+++ FCN/network.py
@@ -8,6 +8,7 @@ import scipy.misc
 from ImSeg_Dataset import ImSeg_Dataset as Data
 import os
 from PIL import Image
+import logging
 
 ## Global variables
 IM_SIZE = [224,224,3]
@@ -17,6 +18,10 @@ learning_rate = 0.0001
 num_epochs = 300
 batch_size = 32
 
+
+## =============================================================================================
+## Basic CNN Operations
+## =============================================================================================
 
 """
 conv_layer(input, filter_shape, stride) creates a new convolutional layer and 
@@ -46,6 +51,9 @@ def batch_norm(x):
   mean, variance = tf.nn.moments(x, axes=[0])
   return tf.nn.batch_normalization(x, mean, variance, None, None, 0.001)
 
+## =============================================================================================
+## Define Res-Net Operations
+## =============================================================================================
 
 """
 resnet_block(..) performs the (non-downsampled) convolutions on the input tensor
@@ -97,6 +105,10 @@ def resnet_block_bottleneck(input_t, filter_shape, stride=[1,1,1,1], padding='SA
   
   return x + identity
 
+## =============================================================================================
+## Define RefineNet Operations
+## =============================================================================================
+
 ## Define Upsampling
 """
 deconv_layer(input, filter_shape, stride) creates a new transpose convolutional layer 
@@ -116,14 +128,10 @@ def deconv_layer(input_t, filter_shape, output_shape, stride=[1,2,2,1], padding=
   biases = tf.Variable(tf.zeros([filter_shape[2]]))
   return tf.nn.conv2d_transpose(input_t, weights, output_shape, stride, padding) + biases
 
-## =============================================================================================
-## Define RefineNet
-## =============================================================================================
-
 """
 Residual Convolution Unit
-Essentially a resnet block without the batch norm.
-Uses 3x3 convolutions (maintains dimensions of input.)
+- Essentially a resnet block without the batch norm.
+- Uses 3x3 convolutions (maintains dimensions of input.)
 Requires:
   input_t: the input tensor
   filter_shape: [filter_height, filter_width, depth_of_input, n_filters]
@@ -143,15 +151,16 @@ def rcu_block(input_t, n_layers=2):
 
 """
 Multi-resolution Fusion.
-Fuses inputs into high-res feature map. First applies convolutions to create feature maps
-of same dimension (smallest depth of channels among inputs).
-Upsamples the smaller feature maps to largest resolution of inputs, then sums them all.
+- Fuses inputs into high-res feature map. First applies 3x3 convolutions to create feature maps
+  of same depth dimension (smallest depth of channels among inputs).
+- Upsamples the smaller feature maps to largest resolution of inputs, then sums them all.
 Requires:
   input_tensors: a list of tensors (usually different dimensions) being inputted to the block.
 """
 def mrf_block(input_tensors):
   
-  # Convolve input tensors using 3x3 filters
+  # Convolve input tensors using 3x3 filters. 
+  # All output tensors will have same channel depth (# of channels)
   convolved = []
   smallest_depth = min(input_tensors, key=lambda t: int(t.get_shape()[3]) )
   smallest_depth = int(smallest_depth.get_shape()[3])
@@ -177,9 +186,10 @@ def mrf_block(input_tensors):
 
 """
 Chained Residual Pooling.
-Chain of multiple pooling blocks, each consisting of one max-pooling layer
-and one convolutional layer. Kernel size for pooling is 5.
-Output feature maps of pooling blocks are summed with identity mappings.
+- Chain of multiple pooling blocks, each consisting of one max-pooling layer
+  and one convolutional layer. Kernel size for pooling is 5.
+- Output feature maps of pooling blocks are summed with identity mappings.
+- Maintains the dimensions of the input.
 Requires:
   input_t is the output of the mrf_block. 
 """
@@ -197,10 +207,10 @@ def crp_block(input_t, n_pool_blocks=2):
 
 """
 RefineNet block.
-Applies Residual Convolution Units twice to each input tensor
-Fuses them together with Multi-Resolution Fusion.
-Applies Chained Residual Pooling
-Applies Residual Convolution Unit once one last time.
+- Applies Residual Convolution Units twice to each input tensor
+- Fuses them together with Multi-Resolution Fusion.
+- Applies Chained Residual Pooling
+- Applies Residual Convolution Unit once one last time.
 Requires:
   input_tensors: A list of tensors to pass through the refine net.
 """
@@ -231,7 +241,7 @@ X = tf.placeholder(tf.float32, shape=[None] + IM_SIZE)
 y = tf.placeholder(tf.float32, shape=[None] + LABEL_SIZE) 
 
 # Downsampling /2
-block_1 = conv_layer(X, [7,7,3,64], stride=[1,2,2,1]) # 2 downsampled
+block_1 = conv_layer(X, [7,7,3,64], stride=[1,2,2,1]) # 1/2 downsampled
 
 # Downsampling /2
 # tf.nn.maxpool ksize is [batch, width, height, channels]. batch and channels is 1
@@ -240,13 +250,13 @@ block_1_pooled = tf.nn.max_pool(block_1, [1,3,3,1], [1,2,2,1], padding='SAME')
 
 block_2 = resnet_block(block_1_pooled, [3,3,64,64])
 block_3 = resnet_block(block_2, [3,3,64,64])
-block_4 = resnet_block(block_3, [3,3,64,64]) # 4 downsampled
+block_4 = resnet_block(block_3, [3,3,64,64]) # 1/4 downsampled
 
 # Downsampling /2
 block_5 = resnet_block_bottleneck(block_4, [3,3,128,128])
 block_6 = resnet_block(block_5, [3,3,128,128])
 block_7 = resnet_block(block_6, [3,3,128,128])
-block_8 = resnet_block(block_7, [3,3,128,128]) # 8 downsampled
+block_8 = resnet_block(block_7, [3,3,128,128]) # 1/8 downsampled
 
 # Downsampling /2
 block_9 = resnet_block_bottleneck(block_8, [3,3,256,256])
@@ -254,12 +264,12 @@ block_10 = resnet_block(block_9, [3,3,256,256])
 block_11 = resnet_block(block_10, [3,3,256,256])
 block_12 = resnet_block(block_11, [3,3,256,256])
 block_13 = resnet_block(block_12, [3,3,256,256])
-block_14 = resnet_block(block_13, [3,3,256,256]) # 16 downsampled
+block_14 = resnet_block(block_13, [3,3,256,256]) # 1/16 downsampled
 
 # Downsampling / 2
 block_15 = resnet_block_bottleneck(block_14, [3,3,512,512])
 block_16 = resnet_block(block_15, [3,3,512,512])
-block_17 = resnet_block(block_16, [3,3,512,512]) # 32 downsampled
+block_17 = resnet_block(block_16, [3,3,512,512]) # 1/32 downsampled
 
 # At size 7x7 at this point.
 
@@ -274,9 +284,11 @@ block_17 = resnet_block(block_16, [3,3,512,512]) # 32 downsampled
 
 # fcn8 = deconv_layer(pool_3_and_4, [3,3,1,128], [batch_size,224,224,1], [1,8,8,1])
 
+
 ## =============================================================================================
 ## Apply Refine-Net 
 ## =============================================================================================
+
 # Refine Net returns result 1/4 the size of input. Still need to upsample 4 times.
 # Expect the depth of the refine net output to be 64, since that is depth of #4 downsampled.
 upsampled = refine_net_block([block_17, block_14, block_8, block_4])
@@ -299,13 +311,14 @@ init = tf.global_variables_initializer()
 
 
 if __name__ == "__main__":
+
+  ## Set up the logger
+  logging.basicConfig(filename="ImSegEval.log")
   
   ## Get the data
   data = Data('../data_path_white_plains_224')
   if not os.path.isdir('../data_path_white_plains_224/im_seg'):
     data = Data.build_dataset()
-
-  
 
   # Begin session
   with tf.Session() as sess: 
@@ -321,30 +334,65 @@ if __name__ == "__main__":
     
     # Number of training samples and number of batches
     num_train = data.data_sizes[0]
-    num_batches = num_train//batch_size
+    num_train_batches = num_train//batch_size
 
-    # Validation size
+    # Number of validation samples and number of batches
     num_val = data.data_sizes[1]
+    num_val_batches = num_val//batch_size
     
     # Training
     for epoch in range(num_epochs):
-      indices = list(range(num_train))
-      np.random.shuffle(indices)
-      # Track epoch loss
-      epoch_loss = 0
-      for batch in range(num_batches):
+
+      # Shuffle indices of training image to randomise batch selection
+      train_indices = list(range(num_train))
+      np.random.shuffle(train_indices)
+
+      val_indices = list(range(num_val))
+      np.random.shuffle(val_indices)
+
+      # Track epoch loss and IoU
+      epoch_train_loss = 0
+      epoch_val_loss = 0
+      epoch_IoU = 0
+
+      ## Training the model and recording training loss
+      for batch in range(num_train_batches):
         
         # Get the batch
-        X_batch, y_batch = data.get_batch(indices[batch*batch_size : (batch+1)*batch_size], "train")
+        X_batch, y_batch = data.get_batch(train_indices[batch*batch_size : (batch+1)*batch_size], "train")
 
-        ## Resize images (unimplemented)
+        ## TODO: Resize images (unimplemented)
 
         # Since it is a dictionary, X (defined above) gets the batch of images X_batch (same for y)
-        _, batch_loss = sess.run([optimizer, loss], feed_dict={X:X_batch, y:y_batch})
+        _, train_loss = sess.run([optimizer, loss], feed_dict={X:X_batch, y:y_batch})
         
-        epoch_loss += batch_loss
+        # Record the training loss
+        epoch_train_loss += train_loss
+        logging.info("Epoch: " + str(epoch+1) + ", Training Loss: " + str(epoch_train_loss))
+
       
-      ## TODO: Save weights at each epoch.
+      ## Recording validation loss
+      for batch in range(num_val_batches):
+
+        # Get the batch
+        X_batch, y_batch = data.get_batch(val_indices[batch*batch_size : (batch+1)*batch_size], "val")
+        
+        ## TODO: Resize images (unimplemented)
+
+        # Get the predictions
+        preds, val_loss = sess.run([result, loss], feed_dict={X:X_batch, y:y_batch})
+        
+        # Record the validation loss
+        epoch_val_loss += val_loss
+        logging.info("Epoch: " + str(epoch+1) + ", Validation Loss: " + str(epoch_val_loss))
+
+        # Calculate IoU for entire image.
+        print(preds)
+       
+      
+      ## TODO: Save weights with checkpoint files.
+
+      # Save predictions
       if (epoch+1)%100 == 0:
 
         X_val_batch, y_val_batch = data.get_batch([i for i in range(32)], "val")
@@ -358,7 +406,8 @@ if __name__ == "__main__":
           img.save(f'epoch{epoch}_{i}.png')
           i += 1
           
-      print(f"Loss at epoch {epoch+1}: {epoch_loss}")
+      print(f"Epoch {epoch+1}, Training Loss: {epoch_train_loss}")
+      print(f"Epoch {epoch+1}, Validation Loss: {epoch_val_loss}")
 
 
 
