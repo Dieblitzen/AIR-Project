401a4b5f0de494bab96ee37d656c8d0153362319
Samar Khanna <sak296@cornell.edu>
2019-05-06 08:48:24 -0400

    Fixed mean calculation

--- FCN/preprocess.py
+++ FCN/preprocess.py
@@ -2,5 +2,8 @@ import numpy as np
 
 ## Calculate mean of dataset.
 def mean_of_data(data):
-  return np.mean(data)
+  mean = 0
+  for im in data:
+    mean += np.mean(im)
+  return mean/float(len(data))
 
