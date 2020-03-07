af6da3f2dbba226b5e64dd2b49cb11b9d58ad015
Samar Khanna <samar.khanna@gmail.com>
2020-03-07 18:31:06 -0500

    Sanity check to make sure tiling and resizing works

--- Drone/scratch.py
+++ Drone/scratch.py
@@ -3,13 +3,13 @@ import argparse
 import numpy as np
 from PIL import Image
 
-## Tile and save image
-def tile_tiff_image(path_to_tiff):
+TILE_SIZE = 224
+
+def read_tiff_image(path_to_tiff):
   im = gdal.Open(path_to_tiff)
   im_arr = im.ReadAsArray()
-  print(im_arr.shape)
   im_arr = im_arr.transpose(1, 2, 0)
-  print(im_arr.shape)
+  # print(im_arr)
 
   # Try and remove NaNs
   im_arr[np.isnan(im_arr)] = -128
@@ -17,10 +17,27 @@ def tile_tiff_image(path_to_tiff):
   clean_arr = im_arr[row_mask, :, :]
   col_mask = (clean_arr[..., 0] > -128).any(axis=0)
   clean_arr = clean_arr[:, col_mask, :]
-  print(clean_arr.shape)
 
-  clean_im = Image.fromarray(clean_arr).convert('RGB')
-  clean_im.save(path_to_tiff.replace('.tiff', '.jpg'))
+  return clean_arr
+  # clean_im = Image.fromarray(clean_arr).convert('RGB')
+  # clean_im.save(path_to_tiff.replace('.tif', '.jpg'))
+
+## Tile and save image
+# Per-pixel resolution goal: 1 pix ~ 1 m
+def tile_image(im_arr, out_path, resolution=3):
+  print(im_arr.shape)
+
+  # Tile according to expanded tile size
+  res = int(100/resolution)
+  size = res * TILE_SIZE
+  print(size)
+
+  tile_arr = im_arr[:size*100, :size*100, :]
+  tile_im = Image.fromarray(tile_arr).convert('RGB')
+  tile_im = tile_im.resize((TILE_SIZE, TILE_SIZE), resample=Image.BICUBIC)
+  print(tile_im.size)
+  tile_im.save(out_path)
+
 
 
 def passed_arguments():
@@ -28,10 +45,12 @@ def passed_arguments():
   parser.add_argument("-p", "--im_path",
                       type=str,
                       required=True,
-                      help="Path to image .tiff file.")
+                      help="Path to image .tif file.")
   args = parser.parse_args()
   return args
 
+
 if __name__ == "__main__":
   args = passed_arguments()
-  tile_tiff_image(args.im_path)
+  im_arr = read_tiff_image(args.im_path)
+  tile_image(im_arr, args.im_path.replace(".tif", ".jpg"), resolution=3)
